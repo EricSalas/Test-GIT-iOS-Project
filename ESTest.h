daeec42e5bcace04147f56a9687adbb7588e0409
Eric Salas <esalas.0691@hotmail.com>
2015-04-14 20:38:05 -0300

    added a test class

---
+++ ESTest.h
@@ -0,0 +1,13 @@
+//
+//  ESTest.h
+//  GitTest
+//
+//  Created by Eric Salas on 4/14/15.
+//  Copyright (c) 2015 Eric Salas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESTest : NSObject
+
+@end
---
+++ ESTest.m
@@ -0,0 +1,13 @@
+//
+//  ESTest.m
+//  GitTest
+//
+//  Created by Eric Salas on 4/14/15.
+//  Copyright (c) 2015 Eric Salas. All rights reserved.
+//
+
+#import "ESTest.h"
+
+@implementation ESTest
+
+@end
